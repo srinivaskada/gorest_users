96a96eedaeeba3317717885a1dffeeabe0d1f434
srinivas <srinivas@surfsolutions.com>
2020-10-11 17:28:49 +0530

    minor change

--- shared/users-service/src/constants.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = {
-  SERVICE_NAME: 'users-service',
-}
--- shared/users-service/src/models/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const User = require('../../../shared/models/user-model')
-module.exports = {
-  User
-}
